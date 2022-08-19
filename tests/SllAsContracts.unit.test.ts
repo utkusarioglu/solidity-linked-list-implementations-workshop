d0d2bbe2f94126f5a0ec3488982cc16028214dae
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-08-19 09:35:46 +0000

    Test(unit): Update import paths on `SllAsContracts`

--- tests/SllAsContracts.unit.test.ts
+++ tests/SllAsContracts.unit.test.ts
@@ -2,8 +2,8 @@ import { abi } from "_artifacts/src/contracts/SllAsContracts.sol/SllNode.json";
 import { beforeEachFacade, expect, testAccounts } from "_services/test.service";
 import { ethers } from "hardhat";
 import { type SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { type SllAsContracts } from "_typechain/SllAsContracts.sol/SllAsContracts";
-import { type SllNode } from "_typechain/SllAsContracts.sol/SllNode";
+import { type SllAsContracts } from "_typechain/SllAsContracts.sol";
+import { type SllNode } from "_typechain/SllAsContracts.sol";
 
 const CONTRACT_NAME = "SllAsContracts";
 
