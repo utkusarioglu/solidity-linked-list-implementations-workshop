9b6acf85cff4d961137ba6667972584c2d8820a6
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-08-20 06:16:53 +0000

    Chore(template): Merge template changes

--- scripts/solc-select-install.sh
+++ scripts/solc-select-install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # gets used solidity version from hardhat.config.ts, installs and enables it for crytic-compile
-echo "Determining solc version from hardhat.config.ts..."
 SOLC_VERSION=$(perl -ne 'print $1 while /solidity: "(.*)"/g' hardhat.config.ts)
+echo "Determined `hardhat.config.ts` to be $SOLC_VERSION"
+echo "Installing the same version for python tools..."
 solc-select install $SOLC_VERSION
 solc-select use $SOLC_VERSION
