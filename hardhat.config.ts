5c4b56bbbd5fe64199614edf4d4672fc9dcb4780
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-09-11 16:58:46 +0300

    Chore(template): Update template
    - This commit pulls the updates from the template (upstream) repo.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -27,7 +27,7 @@ const hardhatConfig = {
   solidity: "0.8.7",
   defaultNetwork: "hardhat",
   paths: {
-    sources: "./src/contracts",
+    sources: "src/contracts",
     tests: "tests",
     cache: "artifacts/cache",
     artifacts: "artifacts/hardhat",
@@ -48,12 +48,12 @@ const hardhatConfig = {
       saveDeployments: true,
       accounts: hardhatAccounts(),
       tags: ["local"],
-      // forking: {
-      //   enabled: true,
-      //   url: `https://polygon-mumbai.g.alchemy.com/v2/${config.get(
-      //     "apiKeys.alchemy.polygon.mumbai"
-      //   )}`,
-      // },
+      forking: {
+        enabled: config.get("features.forking"),
+        url: `https://polygon-mumbai.g.alchemy.com/v2/${config.get(
+          "apiKeys.alchemy.polygon.mumbai"
+        )}`,
+      },
     },
 
     geth1: {
--- scripts/solc-select-install.sh
+++ scripts/solc-select-install.sh
@@ -2,7 +2,7 @@
 
 # gets used solidity version from hardhat.config.ts, installs and enables it for crytic-compile
 SOLC_VERSION=$(perl -ne 'print $1 while /solidity: "(.*)"/g' hardhat.config.ts)
-echo "Determined `hardhat.config.ts` to be $SOLC_VERSION"
+echo "Determined 'hardhat.config.ts' to use solidity version $SOLC_VERSION."
 echo "Installing the same version for python tools..."
 solc-select install $SOLC_VERSION
 solc-select use $SOLC_VERSION
