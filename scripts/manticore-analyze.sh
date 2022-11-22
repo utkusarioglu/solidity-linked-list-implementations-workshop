481ce41739c6e391cf3dc47d8e72346562d8052f
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-11-22 13:37:10 +0000

    Chore(template): Accept repo template changes
    - Update analysis scripts to preprocess contracts through hardhat before
      runs.
    - Add new methods in `analysis-common.sh` in order to service the above
      changes made in analysis scripts.
    - Add telegram notifications to all testing and analysis workflows.
    - Remove commented configs from workflows, these won't ever be used.
    - Remove `export_dir` attribute from `crytic.config.json`. This is done
      because the custom dir causes issues with echidna.

--- scripts/manticore-analyze.sh
+++ scripts/manticore-analyze.sh
@@ -7,13 +7,10 @@ scripts/solc-select-install.sh
 TEMP_SOLC_JSON_PATH="/tmp/solc-settings.json"
 ANALYSIS_LOG_SUFFIX="manticore.log"
 TIMEOUT_PER_CONTRACT=$(( 1 * 60 * 60 )) # 1 hour
+PREPROCESSED_CONTRACTS_PATH=/tmp/contracts
 
 sources_path=$(get_sources_path)
 current_date=$(get_current_date_string)
-
-get_solc_remaps() {
-  jq -r '.settings.remappings | join(" ")' solc.json
-}
 
 # Goes through the contracts in `src/contracts` uses `solc.json` at repo 
 # root for remappings needed for `crytic-compile` to do its thing
@@ -21,17 +18,20 @@ main() {
   artifacts_folder=$(create_artifacts_subfolder "manticore")
   echo "Created artifacts folder: '$artifacts_folder'…"
 
-  contract_names=$(get_contract_names)
+  preprocess_contracts $PREPROCESSED_CONTRACTS_PATH
+  contract_names=$(get_contract_names $PREPROCESSED_CONTRACTS_PATH)
   echo "Found contracts: '$contract_names'"
 
-  solc_remaps=$(get_solc_remaps)
+  solc_remaps=$(get_solc_remaps_joined)
   for contract_name in $contract_names;
   do
     echo "Testing: '$contract_name'…"
     source_contract_path="$sources_path/$contract_name.sol"
     analysis_log_filename="$contract_name-$current_date.$ANALYSIS_LOG_SUFFIX"
     analysis_log_path="$artifacts_folder/$analysis_log_filename"
-    mcore_workspace="$artifacts_folder/$current_date"
+    mcore_workspace="$artifacts_folder/$current_date/$contract_name"
+
+    mkdir -p "$mcore_workspace"
 
     manticore \
       --hardhat-cache-directory "$(pwd)/artifacts/cache" \
